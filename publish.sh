b4f935503fc2d503e2a1761f57478823c8db6512
Wendly Saintil <wspecs@hotmail.com>
2017-02-09 06:05:10 +0000

    latest update

--- publish.sh
+++ publish.sh
@@ -1,10 +1,9 @@
-tmp_readme=''
 ws package update
-ws package describe --text "$(cat tmp_readme)"
+ws package describe # --text "$(cat tmp_readme)"
 rm tmp_readme
 if [ -f .gitignore ]; then
   git pull
   git add --all
   git commit -m 'latest update'
-  # git push
+  git push
 fi
